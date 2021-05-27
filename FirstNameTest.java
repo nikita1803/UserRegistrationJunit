80c0ad254a3ad521704ab1a43c35ff1d6407faa5
nikita1803 <rainikita595@gmail.com>
2021-05-28 02:30:03 +0530

    Added: Last name validation test

--- FirstNameTest.java
+++ FirstNameTest.java
@@ -7,9 +7,15 @@ import org.junit.Test;
 public class FirstNameTest 
 {
 	@Test
-	public void test() {
+	public void firstNameTest() {
 		UserRegisterTest FirstName = new UserRegisterTest();
 		boolean result = FirstName.usernameValidation("Nikki");
+		assertEquals(true,result);
+	}
+	@Test
+	public void lastNameTest() {
+		UserRegisterTest LastName = new UserRegisterTest();
+		boolean result = LastName.usernameValidation("rai");
 		assertEquals(true,result);
 	}
 }
--- UserRegisterTest.java
+++ UserRegisterTest.java
@@ -4,6 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegisterTest 
 {
+	/**
+	 * Test case to validate the first name and last name.
+	 * @return boolean value
+	 */
 	public static boolean usernameValidation(String name) 
 	{
 		String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
@@ -15,4 +19,5 @@ public class UserRegisterTest
 		 Matcher m = p.matcher(name);
 		 return m.matches();	 
 	}
+	
 }
